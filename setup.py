728c2cda02c94129968f19b4c775587a993900dc
Pourush Kumar Gupta <pkg.21p10161@mtech.nitdgp.ac.in>
2024-01-02 15:35:04 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical chatbot',
+    version= '0.0.0',
+    author= 'Pourush Kumar Gupta',
+    author_email= 'pkg.21p10161@mtech.nitdgp.ac.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
